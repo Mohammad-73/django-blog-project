aa1cb3f75d9da8bf0d9667cf3e6c695bd42cc6bd
khalili <m.khalili7310@gmail.com>
2026-02-24 12:33:36 +0330

    Django Admin Panel

---
+++ blogproject/blogapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
+    ordering = ['title', 'publish']
+    list_filter = ['status', 'author']
+    search_fields = ['title', 'description', 'author']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {"slug":['title']}
+    list_editable = ['status']
+    list_display_links = ['author']
